c8e141d48180c671576feb55ee4fbd55d03633b0
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-04 19:36:45 -0800

    Update SkyUnit.h

--- include/SkyUnit.h
+++ include/SkyUnit.h
@@ -1,7 +1,7 @@
 namespace SkyUnit {
     std::vector<std::string> _texts = {};
 
-    const std::vector<std::string>& GetStringVector() {
+    std::vector<std::string>& GetStringVector() {
         return _texts;
     }
 }
