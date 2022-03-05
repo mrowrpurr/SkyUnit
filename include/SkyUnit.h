e8021ccd7fc9c61afc0043631024eb85bacca691
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-04 19:21:05 -0800

    Return reference to string vector for testing

--- include/SkyUnit.h
+++ include/SkyUnit.h
@@ -1,15 +1,7 @@
 namespace SkyUnit {
-    std::vector<std::string> _texts = {};
+    std::vector<std::string*> _texts = {};
 
-    void AddText(std::string text) {
-        return _texts.push_back(text);
-    }
-
-    std::vector<std::string_view> GetTexts() {
-        std::vector<std::string_view> copy = {};
-        for (const auto text : _texts) {
-            copy.push_back(text);
-        }
-        return copy;
+    const std::vector<std::string*>& GetStringVector() {
+        return _texts;
     }
 }
