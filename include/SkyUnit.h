cbbc16b05c12274e33d634fc1915c51456ba9198
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-04 18:32:49 -0800

    Update SkyUnit.h

--- include/SkyUnit.h
+++ include/SkyUnit.h
@@ -1,7 +1,15 @@
 namespace SkyUnit {
-    std::string _hello = "Hi there";
+    std::vector<std::string> _texts = {};
 
-    std::string* GetHello() {
-        return &_hello;
+    void AddText(std::string text) {
+        return _texts.push_back(text);
+    }
+
+    std::vector<std::string_view> GetTexts() {
+        std::vector<std::string_view> copy = {};
+        for (const auto text : _texts) {
+            copy.push_back(text);
+        }
+        return copy;
     }
 }
