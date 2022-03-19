0b672b573a8fad7b0caddd6509605688a3779bf7
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-19 12:31:52 -0700

    Added Boost

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+//#include <cstdlib>
+#include <boost/process.hpp>
 
-int main(int argc, char* argv[]) {
+int main(int, char*) {
+    auto skseLoaderPath = R"("C:\Program Files (x86)\Steam\steamapps\common\Skyrim AE with Creation Kit\skse64_loader.exe")";
+//    auto skseLoaderPath = R"("C:\Program Files (x86)\Steam\steamapps\common\Skyrim AE with Creation Kit\SkyrimSE.exe")";
+//    std::system(skseLoaderPath);
+
+    boost::process::spawn(skseLoaderPath);
+
+//    boost::process::child skyrim(skseLoaderPath);
+//    std::cout << std::format("PID: {}\n", skyrim.id());
+//    std::cout << std::format("Running?: {}\n", skyrim.running());
     std::cout << "Hello, this will boot up Skyrim!";
 }
