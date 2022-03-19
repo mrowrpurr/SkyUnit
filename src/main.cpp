754a1c580251e14b09a58afcb6bf15c5e665c39d
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-19 11:22:18 -0700

    Added FindWeaponNames

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include <format>
+
 #include "RE/C/ConsoleLog.h"
+#include "RE/T/TESObjectWEAP.h"
+
+// TODO - Question for Charmed! - *Filter* / Find Match in a BSString or ? Case insensitive?
+
+std::vector<std::string> FindWeaponNames(const std::string& filter) {
+    std::vector<std::string> weaponNames;
+    auto& allWeapons = RE::TESDataHandler::GetSingleton()->GetFormArray<RE::TESObjectWEAP>();
+    for (const auto& weapon : allWeapons) {
+        auto weaponName = std::string(weapon->GetFullName());
+        if (weaponName.find(filter) != std::string::npos)
+            weaponNames.emplace_back(weaponName);
+    }
+    return weaponNames;
+}
 
 extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = []() {
 	SKSE::PluginVersionData v;
@@ -25,7 +41,16 @@ extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadIn
 
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message){
         if (message->type == SKSE::MessagingInterface::kDataLoaded) {
-            RE::ConsoleLog::GetSingleton()->Print("Hello ladies and jellyspoons!");
+            auto* consoleLog = RE::ConsoleLog::GetSingleton();
+            consoleLog->Print("Hello ladies and jellyspoons!");
+            auto weaponNames = FindWeaponNames("Dagger");
+            if (weaponNames.empty()) {
+                consoleLog->Print("No matching weapons found.");
+            } else {
+                for (const auto& weaponName : weaponNames) {
+                    consoleLog->Print(std::format("Found weapon: {}", weaponName).c_str());
+                }
+            }
         }
     });
 
